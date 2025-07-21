6121f168228faf5de8e5a24593d2d0aaac0b61ca
Fritz Mahnke <fritz@mahnke.tech>
2025-07-21 09:41:56 -0700

    feat(py): make input example use display

--- picomigo-py/src/picomigo/examples/input.py
+++ picomigo-py/src/picomigo/examples/input.py
@@ -5,12 +5,18 @@ from pico.modules import input
 
 
 async def main():
+    display = pico.display()
+
     count = 0
 
     while True:
+        display.fill(0)
+
         print(f'--- {count}')
 
         count += 1
+
+        y = 0
 
         for it in ['button_0', 'button_1']:
             if input.is_on(it):
@@ -18,7 +24,14 @@ async def main():
             else:
                 state = 'off'
 
-            print(f'Switch {it} is: {state}')
+            message = f'Switch {it[-1]} is: {state}'
+            print(message)
+
+            display.text(message, 0, y)
+
+            y += 10
+
+        display.show()
 
         await asyncio.sleep(1)
 
