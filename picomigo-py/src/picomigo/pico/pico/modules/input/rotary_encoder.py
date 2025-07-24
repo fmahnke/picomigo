f57fb1fc1a7a26970505b3c33f502c678e71cbc7
Fritz Mahnke <fritz@mahnke.tech>
2025-07-24 14:42:39 -0700

    feat(py): switches 2: RotaryEncoder class

--- picomigo-py/src/picomigo/pico/pico/modules/input/rotary_encoder.py
+++ picomigo-py/src/picomigo/pico/pico/modules/input/rotary_encoder.py
@@ -1,5 +1,7 @@
 # pyright: reportIndexIssue=false
 
+from typing import Any
+
 from machine import Pin
 from pico.config import switches as config
 from rotary_encoder import RotaryEncoderEvent, RotaryEncoderRP2
@@ -9,8 +11,14 @@ __all__ = [
     'encoder',
 ]
 
+
+class RotaryEncoder(RotaryEncoderRP2):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+
 _encoder_pin_clk = Pin(config['rotary_encoder']['clk'], Pin.IN, Pin.PULL_UP)
 _encoder_pin_dt = Pin(config['rotary_encoder']['dt'], Pin.IN, Pin.PULL_UP)
 _encoder_pin_sw = Pin(config['rotary_encoder']['sw'], Pin.IN, Pin.PULL_UP)
 
-encoder = RotaryEncoderRP2(_encoder_pin_clk, _encoder_pin_dt, _encoder_pin_sw)
+encoder = RotaryEncoder(_encoder_pin_clk, _encoder_pin_dt, _encoder_pin_sw)
