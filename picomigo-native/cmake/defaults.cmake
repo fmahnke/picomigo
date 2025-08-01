d2ca51eb625a9ccbb5b19c46c96d0872431480f0
Fritz Mahnke <fritz@mahnke.tech>
2025-08-01 16:14:26 -0700

    feat(build): add defaults.cmake

---
+++ picomigo-native/cmake/defaults.cmake
@@ -0,0 +1,13 @@
+set(PICO_SDK_PATH $ENV{PICO_SDK_PATH})
+
+# if(NOT DEFINED ENV{CC})
+#     set(CC /usr/bin/gcc-14)
+# endif()
+# 
+# if(NOT DEFINED ENV{CXX})
+#     set(CXX /usr/bin/g++-14)
+# endif()
+
+# if(NOT DEFINED ENV{PICO_BOARD})
+#     set(PICO_BOARD pico2_w)
+# endif()
