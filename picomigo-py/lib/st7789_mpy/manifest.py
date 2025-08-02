dc9c2bde9b17c4d39a53186fb872b63cb27f1867
Fritz Mahnke <fritz@mahnke.tech>
2025-08-01 17:28:07 -0700

    fix(st7789): add missing lib/st7789_mpy files

---
+++ picomigo-py/lib/st7789_mpy/manifest.py
@@ -0,0 +1,5 @@
+# include("$(BOARD_DIR)/manifest.py")
+include("$(PORT_DIR)/boards/manifest.py")
+
+module('vga1_16x32.py', base_path='fonts/bitmap')
+module('vga2_16x32.py', base_path='fonts/bitmap')
