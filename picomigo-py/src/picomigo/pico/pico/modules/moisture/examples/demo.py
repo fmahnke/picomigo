860e9c0aee03899b6d9a0e2eec5fc653fe73b7c7
Fritz Mahnke <fritz@mahnke.tech>
2025-08-05 17:46:31 -0700

    feat(moisture): add moisture module
    wip2
    
    wip

---
+++ picomigo-py/src/picomigo/pico/pico/modules/moisture/examples/demo.py
@@ -0,0 +1,61 @@
+import asyncio
+import time
+
+import pico
+from machine import ADC, PWM, Pin
+
+_tone_min = 100
+_tone_max = 750
+_moisture_threshold_min = 45000
+_moisture_threshold_max = 65535
+_tone_range = _moisture_threshold_max - _moisture_threshold_min
+
+
+class MoistureExample:
+    _input: ADC = ADC(26)
+    _buzzer = PWM(Pin(22))
+    _duty = int(5000)
+    _tone = True
+    _step = 0
+    _next_update = 0
+
+    async def run(self):
+        while True:
+            now = time.time_ns()
+
+            moisture = self._input.read_u16()
+
+            # print(f'moisture: {moisture}')
+
+            if moisture < _moisture_threshold_min:
+                silent = True
+            else:
+                silent = False
+
+            freq = int(
+                _tone_min + (moisture - _moisture_threshold_min) / _tone_range
+                * _tone_max
+            )
+            '''
+            freq = int(
+                _tone_min
+                + (_moisture_max - moisture) / _moisture_max * _tone_max
+            )
+            '''
+
+            if now > self._next_update:
+                print(f'moisture: {moisture}, freq: {freq}, now: {now}')
+
+                self._next_update = now + 100000000
+
+            if self._tone:
+                if silent:
+                    _ = self._buzzer.duty_u16(0)
+                else:
+                    self._buzzer.freq(freq)
+                    _ = self._buzzer.duty_u16(self._duty)
+
+
+moisture_example = MoistureExample()
+
+pico.run(moisture_example.run)
